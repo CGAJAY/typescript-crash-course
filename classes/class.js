c297c3e2fa947ede59b3d9fa35a0aab18f1e5b79
CGAJAY <jmugendi737@gmail.com>
2024-11-22 21:26:40 +0300

    Added access modifiers to control access of properties & methods

--- classes/class.js
+++ classes/class.js
@@ -25,3 +25,56 @@ class Theperson {
 const Person20 = new Theperson("Frank", 18);
 Person20.greetings();
 // ACCESS MODIFIERS IN TYPESCRIPT
+// They determine the visibility & accessibility of properties & methods within a class
+// 1. Public (default)
+// Properties & methods marked as public (or left without a modifier) are accessible from anywhere
+class PublicExample {
+    constructor(name, nickName) {
+        this.name = name;
+        this.nickName = nickName;
+    }
+    greetingss() {
+        console.log(`Hello ${this.name}`);
+    }
+}
+const friend1 = new PublicExample("Flavian", "Kibz");
+console.log(friend1.name); // Flavian
+console.log(friend1.nickName); // Kibz
+friend1.greetingss(); // Hello Flavian
+// The name, nickName and greet() are accessible both inside and outside the class.
+// 2. Private
+// properties & methods marked as private are accessible only within the class where they are defined
+// They can't be accessed outside the class or by derived classes (subclasses)
+class PrivateExample {
+    constructor(secret) {
+        this.secret = secret;
+    }
+    revealSecret() {
+        console.log(`The secret is: ${this.secret}`);
+    }
+}
+const obj33 = new PrivateExample("topSecret");
+// console.log(obj33.secret); // Property 'secret is private
+obj33.revealSecret(); // The secret is: topSecret
+// You can't directly access secret from outside the class, but you can use methods like revealSecret to interact with it indirectly
+// 3. Protected
+// properties and methods marked as protected are accessible within the class and in its subclasses but not from outside the class
+class ProtectedExample {
+    constructor(data) {
+        this.data = data;
+    }
+    showData() {
+        console.log(`Data: ${this.data}`);
+    }
+}
+class DerivedExample extends ProtectedExample {
+    display() {
+        this.showData(); // Accessing the protected method
+        return "hello";
+    }
+}
+let obj4 = new DerivedExample("sensitive Data");
+// console.log(obj4.data); // Property 'data' is protected
+obj4.display(); // Data: sensitive Data
+console.log(obj4.display());
+// The data property is accessible inside DerivedExample but not outside it
--- classes/class.ts
+++ classes/class.ts
@@ -35,3 +35,71 @@ const Person20 = new Theperson("Frank", 18);
 Person20.greetings();
 
 // ACCESS MODIFIERS IN TYPESCRIPT
+// They determine the visibility & accessibility of properties & methods within a class
+
+// 1. Public (default)
+// Properties & methods marked as public (or left without a modifier) are accessible from anywhere
+
+class PublicExample {
+	public name: string;
+	nickName: string;
+	constructor(name: string, nickName: string) {
+		this.name = name;
+		this.nickName = nickName;
+	}
+	public greetingss(): void {
+		console.log(`Hello ${this.name}`);
+	}
+}
+
+const friend1 = new PublicExample("Flavian", "Kibz");
+console.log(friend1.name); // Flavian
+console.log(friend1.nickName); // Kibz
+friend1.greetingss(); // Hello Flavian
+// The name, nickName and greet() are accessible both inside and outside the class.
+
+// 2. Private
+// properties & methods marked as private are accessible only within the class where they are defined
+// They can't be accessed outside the class or by derived classes (subclasses)
+
+class PrivateExample {
+	private secret: string;
+	constructor(secret: string) {
+		this.secret = secret;
+	}
+	public revealSecret(): void {
+		console.log(`The secret is: ${this.secret}`);
+	}
+}
+
+const obj33 = new PrivateExample("topSecret");
+// console.log(obj33.secret); // Property 'secret is private
+obj33.revealSecret(); // The secret is: topSecret
+// You can't directly access secret from outside the class, but you can use methods like revealSecret to interact with it indirectly
+
+// 3. Protected
+// properties and methods marked as protected are accessible within the class and in its subclasses but not from outside the class
+
+class ProtectedExample {
+	protected data: string;
+	constructor(data: string) {
+		this.data = data;
+	}
+	protected showData(): void {
+		console.log(`Data: ${this.data}`);
+	}
+}
+
+class DerivedExample extends ProtectedExample {
+	public display(): string {
+		this.showData(); // Accessing the protected method
+		return "hello";
+	}
+}
+
+let obj4 = new DerivedExample("sensitive Data");
+// console.log(obj4.data); // Property 'data' is protected
+obj4.display(); // Data: sensitive Data
+console.log(obj4.display());
+
+// The data property is accessible inside DerivedExample but not outside it
