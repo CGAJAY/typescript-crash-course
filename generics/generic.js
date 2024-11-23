f5c6c59a3a1ece589c837207e93ae9a761d6715a
CGAJAY <jmugendi737@gmail.com>
2024-11-23 23:35:57 +0300

    generic basics

---
+++ generics/generic.js
@@ -0,0 +1,14 @@
+"use strict";
+console.log("Generics");
+// Generics allow us to write reusable, flexible & type-safe code
+// Let us define components(functions, classes, interfaces) that work with a variety of data types without sacrificing type safety
+// BASIC SYNTAX
+// Generic type parameter is defined using <T> where "T" = Type
+function identity(value) {
+    return value;
+}
+console.log(identity("Hello")); // Hello
+//  TypeScript can often infer the type, so you don’t need to explicitly specify it.
+console.log(identity("Hello")); // Hello
+console.log(identity(420)); // 420
+console.log(identity(420)); // 420
